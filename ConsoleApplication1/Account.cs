05732b7fba7f93e9b258dd035fab9c4acae8a3d7
Adam Vongrej <vongrejadam@gmail.com>
2015-12-14 12:22:48 +0100

    Initial commit

---
+++ ConsoleApplication1/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+
+namespace ConsoleApplication1
+{
+    class Account
+    {
+        private double balance;
+
+        public Account(double balance) {
+            this.balance = balance;
+        }
+
+        public void deposit(double amount) {
+            Contract.Requires(amount > 0, "The amount has to be always positive.");
+            balance += amount;
+            Contract.Ensures(balance > 0, "The balance has to be always positivie.");
+        }
+
+        public void withdraw(double amount){
+            Contract.Requires(amount > 0, "The withdrawal amount has to be positive.");
+            Contract.Requires<MaximumAmountException>(amount < balance, "The widthdrawal amount cannot exceed the account balance.");
+            Contract.Ensures(balance < 0, "The account balance cannot be negative");
+            balance -= amount;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(100);
+            acc.deposit(200);
+            acc.withdraw(100);
+        }
+    }
+}
